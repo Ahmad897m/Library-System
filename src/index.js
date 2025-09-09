9df44f635f863952b797b35967d648c23b2c4cd2
Ahmad897m <ahmadmustafa897m@gmail.com>
2025-09-09 17:33:43 +0400

    one more again

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter basename="/Library-System">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
